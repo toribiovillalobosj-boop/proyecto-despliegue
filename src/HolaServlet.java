180a0a6c2aeeca18c9ad0a811d575fe6129d47e4
Rodrigo Martín <b08h16127d@canariaseducacion.es>
2026-06-02 02:10:36 -0400

    v1.1

--- src/HolaServlet.java
+++ src/HolaServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class HolaServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("mensaje", "¡Hola desde el Servlet de rodrigo!");
+        request.setAttribute("mensaje", "¡Hola desde el Servlet de rodrigo modificando WAR desde la práctica 04-06.07!");
         request.getRequestDispatcher("/hola.jsp").forward(request, response);
     }
 }
